8a2c0428284263e092e4fd8d50a5558368115666
Deyan Yonkov <deqnyonkov47@gmail.com>
2025-02-01 18:44:55 +0200

    Refractor postgre usage

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,23 +1,22 @@
- import { db } from "@vercel/postgres";
+import postgres from "postgres";
 
- const client = await db.connect();
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
- async function listInvoices() {
- 	const data = await client.sql`
+async function listInvoices() {
+  const data = await sql`
      SELECT invoices.amount, customers.name
      FROM invoices
      JOIN customers ON invoices.customer_id = customers.id
      WHERE invoices.amount = 666;
    `;
 
- 	return data.rows;
- }
+  return data;
+}
 
 export async function GET() {
-  
-   try {
-   	return Response.json(await listInvoices());
-   } catch (error) {
-   	return Response.json({ error }, { status: 500 });
-   }
+  try {
+    return Response.json(await listInvoices());
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
 }
